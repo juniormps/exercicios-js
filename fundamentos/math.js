eb1f3e739349eea821f4f2f9faead32bd5ae616f
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2023-12-13 13:22:44 -0300

    Create math.js
    exemplo de objeto Math

---
+++ fundamentos/math.js
@@ -0,0 +1,8 @@
+//O "math" é um objeto para efetuar operações matemáticas.
+//Equipara-se mais ou menos à biblioteca "Matemática - Mat" no portugol
+
+const raio = 5.6
+const area = Math.PI * Math.pow(raio, 2)  //A expressão "Math.pow(raio, 2)" fará a potência "raio²"
+
+console.log(area)
+console.log(typeof Math)

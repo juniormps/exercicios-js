14838b4bd980f4a1238010507f0b4c9115835e3e
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2024-03-04 18:14:44 -0300

    Update templatestrings.js

--- fundamentos/templatestrings.js
+++ fundamentos/templatestrings.js
@@ -6,4 +6,9 @@ const concatenacao = 'Olá ' + nome + ' !'
 const template = `
     Olá
     ${nome}`
-console.log(concatenacao, template)
+console.log(concatenacao, template)  //O template strings suporta quebras de linha, recuos e espaços em branco.
+
+console.log(`1 + 1 = ${1 + 1}`)  //O template strings tbm suporta expressões
+
+const up = texto => texto.toUpperCase()
+console.log(`Ei... ${up('cuidado')}!`)  //Exemplo de uma função Arrow para mostrar que o template strings tbm suporta funções

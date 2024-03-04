d5a858c3f5819daf64af2b79b0aedc0eae3389e7
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2024-03-04 16:33:09 -0300

    Create templatestrings.js

---
+++ fundamentos/templatestrings.js
@@ -0,0 +1,9 @@
+//Aula de TEMPLATE STRINGS
+//Template Strings é a forma mais moderna de concatenar strings e expressões
+
+const nome = 'Rebeca'
+const concatenacao = 'Olá ' + nome + ' !'
+const template = `
+    Olá
+    ${nome}`
+console.log(concatenacao, template)

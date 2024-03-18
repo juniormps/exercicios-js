6e1bdaced5d53a4fae9c9eb6bc5c0aafa90ea0eb
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2024-03-18 13:44:34 -0300

    Create usandoVarEmLoop1.js

---
+++ fundamentos/usandoVarEmLoop1.js
@@ -0,0 +1,7 @@
+//Aula sobre o uso de declaração de variável com "VAR" em uma estrutura de repetição LOOP.
+
+for (var i = 0; i < 10; i++) {
+    console.log(i)
+}
+
+console.log('i =', i)  //a variável "i" sai da estrutura de repetição com o valor 10.

c07e220d07fe36ad1ab636182ae24f813a4b9444
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2024-03-25 12:21:49 -0300

    Create atribuicao.js

---
+++ fundamentos/atribuicao.js
@@ -0,0 +1,20 @@
+//Operadores de Atribuição
+
+//"=" operador de atribuição "RECEBE"
+const a = 7
+let b = 3 
+
+b += a  // b = b + a
+console.log(b)
+
+b -= a  // b = b - a
+console.log(b)
+
+b *= 2  // b = b * 2
+console.log(b)
+
+b /= 2  // b = b / 2
+console.log(b)
+
+b %= 2  // b = b % 2
+console.log(b)

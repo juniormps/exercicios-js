2d4f21e48310512814951a57f3e6c1834ef574cb
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2024-04-01 16:46:08 -0300

    Create opUnarios.js

---
+++ fundamentos/opUnarios.js
@@ -0,0 +1,15 @@
+//OPERADORES UNÁRIOS
+
+let num1 = 1
+let num2 = 2
+
+num1++              //operador de incremento pós-fixado
+console.log(num1)
+--num1              //operador de decremento pré-fixado
+console.log(num1)
+
+console.log(++num1 === num2--)
+console.log(num1 === num2)
+
+
+//Além dos operadores de incremento e decremento, existe também o operador de NEGAÇÃO "!", já visto anteriormente.

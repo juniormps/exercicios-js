cea90a9eb15d58154d84a7860a1d4d8bb336e9d6
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2024-04-05 17:19:55 -0300

    Create while.js

---
+++ controle/while.js
@@ -0,0 +1,17 @@
+//Estrutura de Repetição - "WHILE" (ENQUANTO)
+
+//mais indicada para quando o número de repetições for indeterminado
+
+function getInteiroAleatorioEntre(min, max) {
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+
+let opcao = 0
+
+while (opcao != -1) {
+    opcao = getInteiroAleatorioEntre(-1, 10)
+    console.log(`A opção escolhida foi ${opcao}`)
+}
+
+console.log('Até a próxima!')

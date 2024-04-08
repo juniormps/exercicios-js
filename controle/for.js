85d57f6d0901c9ef7d86d94377e9ede7b974513f
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2024-04-08 10:40:27 -0300

    Create for.js

---
+++ controle/for.js
@@ -0,0 +1,16 @@
+//Estrutura de Controle - "FOR" - (PARA)
+
+//EXEMPLO 01
+for(let i = 1; i <= 10; i++) {
+    console.log(`i = ${i}`)
+}
+
+
+//EXEMPLO 02
+const notas = [6.7, 7.4, 9.8, 8.1, 7.7]
+
+for(let i = 0; i < notas.length; i++) {   //LENGTH - traz o comprimento do array
+    console.log('Nota = ', notas[i])
+}
+
+

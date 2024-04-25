8dcf77a94bf9344f513719d996278d9964d6459d
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2024-04-25 11:23:05 -0300

    Update exercicio002.js

--- exercicios-propostos/exercicio002.js
+++ exercicios-propostos/exercicio002.js
@@ -1,3 +1,5 @@
+//Função recebe a idade de um usuário em anos e retorna a mesma em dias.
+
 function converteAnosParaDias(idadeAnos) {
     let idadeDias = idadeAnos * 365
     console.log('A idade digitada em dias é:', idadeDias, 'dias.')

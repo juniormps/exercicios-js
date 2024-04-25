f8020c921a8e812af9315144b487efe1785dfc1d
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2024-04-25 11:56:07 -0300

    Update exercicio003.js

--- exercicios-propostos/exercicio003.js
+++ exercicios-propostos/exercicio003.js
@@ -6,3 +6,17 @@ function calculoSalario(horasTrab, valorHora) {
 }
 
 calculoSalario(160, 32.3)
+
+
+//---------------------------------------------------------------------------------//
+
+//Função recebe a quantidade de horas trabalhadas de um funcionário e quanto ele recebe por hora de trabalho. Retorna o salário mensal descontado 30% de imposto.
+
+let salarioFinal = (horasTrab, valorHora, imposto = 0.3) => {
+    let salarioBase = horasTrab * valorHora
+    let desconto = salarioBase * imposto
+    let salarioMensal = salarioBase - desconto 
+    return salarioMensal
+}
+
+console.log('\nSalário igual a R$', salarioFinal(100, 15.5))

37453d74df612cc608b4fcb84971d6cb0781b5c1
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2024-04-29 13:44:24 -0300

    Create exercicio012.js

---
+++ exercicios-propostos/exercicio012.js
@@ -0,0 +1,18 @@
+//Você escreverá uma função que recebe um objeto como primeiro parâmetro e, como segundo parâmetro, o nome de uma propriedade contida nesse objeto. Em seguida, retorne uma cópia desse objeto sem a propriedade especificada no segundo parâmetro.
+
+function removerAtributo(objeto, atributo) {
+    const copiaObjeto = {...objeto}
+    delete copiaObjeto[atributo]
+    return copiaObjeto
+}
+
+const pessoa = {
+    nome: 'Marcio',
+    ano_nascimento: 1987,
+    naturalidade: 'RJ',
+    nacionalidade: 'Brasileiro'
+}
+
+console.log(removerAtributo(pessoa, pessoa.ano_nascimento))
+
+//A CHAMADA DA FUNÇÃO NÃO ESTÁ FUNCIONANDO

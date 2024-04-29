8d3ba3e52e60d88aae78eca02316af494e8b1464
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2024-04-29 15:30:42 -0300

    Create exercicio014.js

---
+++ exercicios-propostos/exercicio014.js
@@ -0,0 +1,18 @@
+//Desenvolva uma função que recebe como parâmetro um objeto e retorne um array de arrays, em que cada elemento é um array formado pelos pares chave/valor que corresponde a um atributo do objeto. Observe os exemplos abaixo para um melhor entendimento:
+
+function objetoParaArray(objeto) {
+    const arrayCliente = []
+    for(let atributo in objeto) {
+        arrayCliente.push([atributo, objeto[atributo]])
+    }
+    return arrayCliente
+}
+
+const cliente = {
+    nome: 'Samuel',
+    idade: 10,
+    ano_nascimento: 2014,
+    nacionalidade: 'Brasileiro'
+}
+
+console.log(objetoParaArray(cliente))

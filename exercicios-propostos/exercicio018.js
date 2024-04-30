59b617e5c87b898ecb925b273c9f214f879fcaeb
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2024-04-30 17:10:41 -0300

    Update exercicio018.js

--- exercicios-propostos/exercicio018.js
+++ exercicios-propostos/exercicio018.js
@@ -1,9 +1,20 @@
 //Você está trabalhando numa aplicação pessoal de controle de despesas. Na tela principal dessa aplicação, é possível adicionar produtos ou serviços, informando nome, categoria e preço. Uma funcionalidade que você está desenvolvendo no momento é a de somar o total das despesas. Crie uma função que receba um array de produtos e retorne o total das despesas.
 
-function despesasTotais(objeto) {
-
+function despesasTotais(despesas) {
+    let soma =  0
+    for(let i in despesas) {
+        soma += despesas[i].preco
+    }
+    return soma
 }
 
-const produto = {
-    nome:
-}
+const itens = [
+    {nome: 'TV à Cabo', categoria: 'Entretenimento', preco: 120.00},
+    {nome: 'Telefone', categoria: 'Comunicação', preco: 59.00},
+    {nome: 'Internet', categoria: 'Comunicação', preco: 99.00}
+]
+
+console.log(despesasTotais(itens))
+
+
+

880e71f12678a90b534cc7e48c8ae5249b9e1a5d
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2024-04-30 10:46:00 -0300

    Create desafioFuncaoConstrutora.js

---
+++ funcao/desafioFuncaoConstrutora.js
@@ -0,0 +1,28 @@
+//Transformar o exemplo abaixo (que utiliza classe) em uma função construtora:
+
+//Exemplo usando CLASSE
+class Pessoa {
+    constructor(nome) {
+        this.nome = nome
+    }
+
+    falar() {
+        console.log(`Meu nome é ${this.nome}`)
+    }
+}
+
+const p1 = new Pessoa('João')
+p1.falar()
+
+//Resolução com Função Construtora
+function Pessoa {
+    this.nome = nome
+
+    this.falar = function() {
+        console.log(`Meu nome é ${this.nome}`)
+    }
+}
+
+const p1 = new Pessoa('João')
+p1.falar()
+

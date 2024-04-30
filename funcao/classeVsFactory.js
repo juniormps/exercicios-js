1c161400145f99c28e90fbd61ea274a246b00480
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2024-04-30 10:26:18 -0300

    Create classeVsFactory.js

---
+++ funcao/classeVsFactory.js
@@ -0,0 +1,26 @@
+//Classe Vs Função Factory
+
+//Usando CLASSE
+class Pessoa {
+    constructor(nome) {
+        this.nome = nome
+    }
+
+    falar() {
+        console.log(`Meu nome é ${this.nome}`)
+    }
+}
+
+const p1 = new Pessoa('João')
+p1.falar()
+
+
+//Usando FACTORY
+const criarPessoa = nome => {
+    return {
+        falar: () => console.log(`Meu nome é ${nome}`)
+    }
+}
+
+const p2 = criarPessoa('Marcio')
+p2.falar()

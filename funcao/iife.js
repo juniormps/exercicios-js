89833fb90033ff877b550aa7f9d5720e6d8c1c7b
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2024-04-30 11:12:15 -0300

    Create iife.js

---
+++ funcao/iife.js
@@ -0,0 +1,10 @@
+//IIFE -> Immediately Invoked Function Expression
+
+//É uma função que é executada imediatamente após sua criação.
+//Ela deve estar entre parênteses e ser seguida por um par de parênteses tbm, conforme a sintaxe abaixo.
+
+//EXEMPLO
+(function() {
+    console.log('Será executado na hora!')
+    console.log('Foge do escopo mais abrangente!')
+})()

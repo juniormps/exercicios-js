5727d597229e7caa2efa32f8db94713ee7f0ce19
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2024-05-03 15:38:06 -0300

    Create oo.js

---
+++ objeto/oo.js
@@ -0,0 +1,36 @@
+//CÓDIGO NÃO EXECUTÁVEL
+
+
+//Paradigma de programação estruturada:
+
+// --> Procedural: foco em funções. Funções manipulam dados.
+//ex.:
+procedural(valor1, valor2, valor3)
+
+// --> Orientação à objeto (OO): dados que contêm funções. O foco passou a ser em Objetos.
+//Objetos podem conter valores/atributos e comportamentos/funções
+//ex.:
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        //...
+    }
+}
+
+objeto.processamento()
+
+
+//Princípios importantes da orientação à objeto:
+
+//1. Abstração: saber mapear e traduzir um objeto da vida real para o meio virtual de acordo com as necessidades do negócio.
+//ex.: um objeto CARRO deve conter atributos diferentes em um sistema feito para o DETRAN e outro feito para uma oficina mecânica.
+
+//2. Encapsulamento: muitos detalhes de implementação não devem e nem precisam ser expostos aos usuários. 
+
+//3. Herança (Prototype): um objeto filho herda características do objeto pai, avô e assim sucessivamente.
+
+//4. Composição: um objeto é quebrado em outros objetos menores.
+
+//5. Polimorfismo: um conceito abstrato aponta para um conceito concreto.

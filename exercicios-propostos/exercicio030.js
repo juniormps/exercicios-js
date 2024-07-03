58a72fd90f6e52a85890548f03ce0d99aae4f83b
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2024-07-03 19:49:31 -0300

    Update exercicio030.js

--- exercicios-propostos/exercicio030.js
+++ exercicios-propostos/exercicio030.js
@@ -1,12 +1,17 @@
 //Elabore uma função que recebe um objeto com estudantes e suas notas. As notas de cada estudante estarão num array, conforme exemplo abaixo. Você deverá calcular a média da nota de cada aluno e retornar um objeto com os atributos nome e media, que indica o aluno que teve o melhor desempenho nas notas.
 
-function mediaAluno(obj) {
-    const resultado = Object.entries(obj)
-    resultado.forEach(element => {
-        let media = element[1]  
-    });
+const soma = array => array.reduce((acumulador, atual) => acumulador + atual, 0)
+const media = array => soma(array) / array.length
 
-    return resultado
+function recerberMelhorEstudante(estudantes) {
+    const estudantesComMedias = Object.entries(estudantes).map( estudante => {
+    const chave = 0,
+    valor = 1
+    return { nome: estudante[chave], media: media(estudante[valor]) }
+    })
+    const estudantesOrdenados = estudantesComMedias.sort( (estudanteA, estudanteB) => estudanteB.media - estudanteA.media )
+    const melhorEstudante = estudantesOrdenados[0]
+    return melhorEstudante
 }
 
 const notasAlunos = {
@@ -15,4 +20,4 @@ const notasAlunos = {
     Carla: [7, 7, 8, 9]
 }
 
-console.log(mediaAluno(notasAlunos))
+console.log(recerberMelhorEstudante(notasAlunos))

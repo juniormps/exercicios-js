e570ca2f1581a57931ee9a3b701a7b3b16e6b9ef
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2024-08-02 09:05:05 -0300

    Create concat.js

---
+++ array/concat.js
@@ -0,0 +1,10 @@
+//O método "concat" mescla dois ou mais arrays.
+
+const filhas = ['Ualeskah', 'Cibalena']
+const filhos = ['Uoxiton', 'Uesclei']
+const todos = filhas.concat(filhos)
+console.log(todos, filhas, filhos)
+
+console.log([].concat([1, 2], [3, 4], 5, [[6, 7]]))
+
+//O método "concat" pode ainda adicionar novos elementos a um array.

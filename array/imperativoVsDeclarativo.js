5e7b8bb964b55b1753bda600ab63efa32789964a
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2024-08-02 08:58:49 -0300

    Create imperativoVsDeclarativo.js

---
+++ array/imperativoVsDeclarativo.js
@@ -0,0 +1,22 @@
+//Imperativo Vs Declarativo
+
+//No modo IMPERATIVO, a maneira como deve atingir um objetivo é mais importante. Já no modo DECLARATIVO, o objetivo é o mais importante.
+
+const alunos = [
+    { nome: 'João', nota: 7.9 },
+    { nome: 'Maria', nota: 9.2 }
+]
+
+// Imperativo
+let total1 = 0
+for (let i = 0; i < alunos.length; i++) {
+    total1 += alunos[i].nota
+}
+console.log(total1 / alunos.length)
+
+// Declarativo
+const getNota = aluno => aluno.nota
+const soma = (total, atual) => total + atual
+
+const total2 = alunos.map(getNota).reduce(soma)
+console.log(total2 / alunos.length)

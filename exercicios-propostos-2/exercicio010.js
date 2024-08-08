5b527f4c2e25cde4008cc82f378ae47689d81df0
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2024-08-08 16:08:44 -0300

    Create exercicio010.js

---
+++ exercicios-propostos-2/exercicio010.js
@@ -0,0 +1,7 @@
+//10) Crie uma função que verifica se um número inteiro passado como parêmetro é divisível por 3 e retorne true ou false.
+
+function divisivelPor3(num) {
+    return num % 3 === 0
+}
+
+console.log(divisivelPor3(24))

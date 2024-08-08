43e93fac019b182eacc4ca682a470f900c62cc1d
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2024-08-08 16:04:27 -0300

    Create exercicio009.js

---
+++ exercicios-propostos-2/exercicio009.js
@@ -0,0 +1,20 @@
+//09) Construa uma função para um sistema de notas de uma instituição que possui a seguinte política de classificação: Todo aluno recebe uma nota de 0 a 100. Alunos com nota abaixo de 40 são reprovados. As notas possuem a seguinte regra de arredondamento: Se a diferença entre a nota e o próximo múltiplo de 5 for menor que 3, arredondar a nota para esse próximo múltiplo de 5. Se a nota for abaixo de 38, não é feito nenhum arredondamento pois esta nota resulta na reprovação do aluno. Por exemplo, a nota 84 será arredondada para 85, mas a nota 29 não será arredondada por ser abaixo de 40 e não ser possível arredondamento eficiente, ou seja, que evite a reprovação do aluno. No caso de a nota ser 38, o arredondamento é possível pois atingirá 40 e o aluno será aprovado.
+
+function aprovadoOuNao(nota) {
+    if (nota < 38) {
+        return `Nota final: ${nota} - REPROVADO`
+    } else {
+        let diferenca = 5 - nota % 5
+        let notaFinal = 0
+
+        if (diferenca < 3) {
+            notaFinal = nota + diferenca
+        } else {
+            notaFinal = nota
+        }
+        return `Nota final: ${notaFinal} - APROVADO`
+    }
+}
+
+let notaAluno = 38
+console.log(aprovadoOuNao(notaAluno))

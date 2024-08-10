99b6dbf37b30bdab0da127de5379be77aba5de22
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2024-08-09 22:25:19 -0300

    Create exercicio023.js

---
+++ exercicios-propostos-2/exercicio023.js
@@ -0,0 +1,24 @@
+//23) Escreva um algoritmo que leia o código de um aluno e suas três notas. Calcule a média ponderada do aluno, considerando que o peso para a maior nota seja 4 e para as duas restantes, 3. Mostre o código do aluno, suas três notas, a média calculada e uma mensagem "APROVADO" se a média for maior ou igual a 5 e "REPROVADO" se a média for menor que 5. Repita a operação até que o código lido seja negativo.
+
+function Aluno(codigo, nome, nota1, nota2, nota3) {
+    this.codigo = codigo
+    this.nome = nome
+    this.nota1 = nota1
+    this.nota2 = nota2
+    this.nota3 = nota3
+
+    this.notas = [nota1, nota2, nota3].sort()
+
+    this.getMedia = notas => ((notas[0] * 3) + (notas[1] * 3) + (notas[2] * 4)) / (3 + 3 + 4)
+
+    this.getResultadoFinal = media => media >= 5 ? "APROVADO" : "REPROVAO"
+}
+
+const a1 = new Aluno(1001, 'Marcio Junior', 7, 8, 6)
+const a2 = new Aluno(1002, 'Samuel Rocha', 8, 9, 10)
+const a3 = new Aluno(1003, 'José Rocha', 7, 6, 10)
+const a4 = new Aluno(1004, 'Malaquias Azevedo', 5, 4, 3)
+
+
+console.log(a1.getResultadoFinal(a1.getMedia(a1.notas)))
+

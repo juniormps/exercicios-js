9cac58b8204df5fd70f5f020e05edb7b39a3e2b8
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2024-08-20 17:37:01 -0300

    Update exercicio037.js

--- exercicios-propostos-2/exercicio037.js
+++ exercicios-propostos-2/exercicio037.js
@@ -11,25 +11,32 @@ function progressaoAritmetica(numTermos, a1, razao) {
         termo += razao
     }
 
-    return `Os termos da PA são:\n${termosPA} \nA soma dos termos é igual a: ${somaNTermos}`
+    return `Os termos da PA são:\n${termosPA.join(', ')} \nA soma dos termos é igual a: ${somaNTermos}`
 }
 
-function progressaoGeomtrica(numTermos, a1, razao) {
-    const somaNTermos = (numTermos / 2) * (2 * a1 + ((numTermos - 1) * razao))
+function progressaoGeometrica(numTermos, a1, razao) {
+    let somaNTermos = 0
+    if (razao === 1) {
+        somaNTermos = a1 * numTermos
+    } else {
+        somaNTermos = a1 * (((razao**numTermos) - 1) / (razao - 1))
 
-    const termosPA = []
+    }
+
+    const termosPG = []
     let termo = a1
     
     for (let c = 1; c <= numTermos; c++) {
-        termosPA.push(termo)
-        termo += razao
+        termosPG.push(termo)
+        termo *= razao
     }
 
-    return `Os termos da PA são:\n${termosPA} \nA soma dos termos é igual a: ${somaNTermos}`
+    return `Os termos da PG são:\n${termosPG.join(', ')} \nA soma dos termos é igual a: ${somaNTermos}`
 }
 
 const valorProgressaoAritmetica = progressaoAritmetica(5, 2, 3)
-const valorProgressaoGeometrica = progressaoGeomtrica(5, 2, 3)f
-''
-console.log(valorProgressao)
+const valorProgressaoGeometrica = progressaoGeometrica(5, 2, 1)
+
+console.log(valorProgressaoAritmetica)
+console.log(valorProgressaoGeometrica)
 

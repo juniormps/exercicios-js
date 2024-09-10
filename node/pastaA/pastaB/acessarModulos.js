d0e7138d01d22148fa7ccf6595961325a6755f29
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2024-09-10 17:00:03 -0300

    update

--- node/pastaA/pastaB/acessarModulos.js
+++ node/pastaA/pastaB/acessarModulos.js
@@ -1,15 +1,21 @@
 //Como acessar Módulos
 
-//Cada "../" você volta uma pasta
+//Impotando um módulo de algum diretório do projeto. Cada "../" você volta uma pasta.
 const moduloA = require('../../moduloA')  //Aqui voltou para a "pastaA", depois para a pasta "node"
 console.log(moduloA.ola)
 
+
+//Importando um módulo de uma biblioteca que eu mesmo criei e coloquei na pasta "node_moules"
 const saudacao = require('saudacao')
 console.log(saudacao.ola)
 
-const c = require('./pastaC')
+
+//Importando de um diretório que está no mesmo nível. Utiliza-se "./nomeDaPasta".
+const c = require('./pastaC')  //O arquivo "index" já é buscado por default, sem precisar escrevê-lo.
 console.log(c.ola2)
 
+
+//Importando um módulo que já vem previamente instalado junto com o Node (módulos core)
 const http = require('http')
 http.createServer((req, res) => {
     res.write('Bom dia!')

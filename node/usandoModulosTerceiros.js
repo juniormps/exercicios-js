170d5e62c1983d81e74a6c2633143e21675ed29f
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2024-09-10 15:54:31 -0300

    utilizando módulos de terceiros

---
+++ node/usandoModulosTerceiros.js
@@ -0,0 +1,4 @@
+//Importando módulos de terceiros (bibliotecas)
+
+const _ = require('lodash')  //Não é necessário colocar o caminho das pastas
+setInterval(() => console.log(_.random(50, 60)), 2000)

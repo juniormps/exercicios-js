1c342d5c8dd2a6d018af59e06465f83c64262e91
Marcio Junior <105465766+juniormps@users.noreply.github.com>
2024-09-12 17:47:55 -0300

    passando parâmetros

--- node/passandoParametros.js
+++ node/passandoParametros.js
@@ -1,5 +1,3 @@
-//Passando parâmetros para uma função importada de outro módulo.
-
 module.exports = function(...nomes) {
     return nomes.map(nome => `Boa semana ${nome}!`)
 }
---
+++ node/passandoParametrosCliente.js
@@ -0,0 +1,4 @@
+//Passando parâmetros para uma função importada de outro módulo.
+
+const saudacoes = require('./passandoParametros')('Ana', 'Lucas', 'João')  //Aqui estão os parâmetros
+console.log(saudacoes)
